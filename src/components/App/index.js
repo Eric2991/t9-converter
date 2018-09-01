861d62cf25e881e1a0fc820c1c1ae13af845f5a2
eric2991 <eyamane21@gmail.com>
2018-09-01 15:34:15 -0500

    Added initial set-up of App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { hot } from "react-hot-loader"
+import './style.scss'
 
 class App extends React.Component {
-
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
 }
 
-export default App
+export default hot(module)(App)

2c148c1dc6c5ec7613f322149b96657e6e814c3c
eric2991 <eyamane21@gmail.com>
2018-09-02 23:54:34 -0500

    Turned App component into functional component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
-import { hot } from 'react-hot-loader'
 import NumberInput from '../NumberInput'
 import WordList from '../WordList'
 import './style.scss'
 
-type Props = {}
+const App = () => (
+  <div className="App">
+    <header>
+      <h1>T9 Converter</h1>
+      <p>Play around with it!</p>
+    </header>
+    <WordList />
+    <NumberInput />
+  </div>
+)
 
-class App extends React.Component<Props> {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <h1>T9 Converter</h1>
-          <p>Play around with it!</p>
-        </header>
-        <WordList />
-        <NumberInput />
-      </div>
-    )
-  }
-}
-
-export default hot(module)(App)
+export default App

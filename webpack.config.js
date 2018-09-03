b9577e18269f15a971d1db4b245d3da5883e0ffe
eric2991 <eyamane21@gmail.com>
2018-09-02 20:19:05 -0500

    Modified webpack to recognize jsx file types

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,51 @@
-const path = require("path")
-const webpack = require("webpack")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.jsx',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader',
-        options: { presets: ['env', 'flow'], plugins: ["transform-class-properties"] }
+        options: {
+          presets: ['env', 'flow'],
+          plugins: ['transform-class-properties']
+        }
       },
       {
         test: /\.scss$/,
         exclude: /(node_modules)/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
         ]
       }
     ]
   },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, 'dist/'),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
+    publicPath: '/',
     hotOnly: true
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
+      filename: devMode ? '[name].css' : '[name].[hash].css'
     })
-  ]
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 }
